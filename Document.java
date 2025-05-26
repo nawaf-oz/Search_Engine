5f9a3456595137e2e678429c3c1f6abd703622bd
nawaf-oz <159358835+nawaf-oz@users.noreply.github.com>
2025-05-26 22:01:57 +0300

    Create Document.java

---
+++ Document.java
@@ -0,0 +1,9 @@
+public class Document {
+    LinkedList<String>Word=new LinkedList<String>();
+    int ID;
+    String Content;
+    public Document(int id,LinkedList<String>Words,String Contents) {
+        ID=id;
+        Word=Words;
+        Content=Contents;
+    }}
